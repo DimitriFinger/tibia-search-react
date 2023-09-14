b24cdeb65532b57668be92498036f78c51c4cef7
Dimitri Finger <dimitri.finger@outlook.com>
2023-09-14 02:06:33 -0300

    CSS changes, favicon and project cleaning

---
+++ app/components/SearchComponent.jsx
@@ -0,0 +1,42 @@
+'use client'
+
+import React, { useState } from 'react'
+
+export default function SearchComponent() {
+
+    const [nickname, setNickname] = useState('');
+
+    const handleSearch = (e) => {
+        console.log(nickname);
+        e.preventDefault();
+    }
+
+    return (
+        <>
+            <div className="p-8 inline-block flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 ">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-5 text-center text-3xl font-bold ">
+                        TIBIA SEARCH
+                    </h2>
+                </div>
+                <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form action="#">
+                        <div>
+                            <div className="mt-2 flex justify-center">
+                                <input
+                                    onChange={(e) => setNickname(e.target.value)}
+                                    placeholder='Enter character name...'
+                                    className="pl-2.5 block w-3/4 rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-gray-400 focus:ring-green-400 focus:ring-2 focus:outline-none hover:ring-green-400 hover:ring-2 placeholder:text-gray-400 sm:text-sm sm:leading-6" />
+                            </div>
+                        </div>
+                        <div className='flex justify-center'>
+                            <button onClick={handleSearch} className="mt-5 w-1/3 justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">
+                                Search
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
